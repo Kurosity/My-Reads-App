f8d59ca49a526c1cca4647bbab881b4b664b4cdf
Kurosity <40587453+Kurosity@users.noreply.github.com>
2018-11-26 22:58:56 -0500

    Removed console.log

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,7 +8,6 @@ import {Link} from 'react-router-dom'
 class Main extends Component{
 
     render(){
-        console.log(this.props.books)
         return(            
             <div className="list-books">
                 <div className="list-books-title">
